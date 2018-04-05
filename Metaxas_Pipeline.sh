fea3e760d89fd3786cc8765de6fca326d8808256
jwestrob <cobe44@gmail.com>
2018-04-05 16:00:14 -0400

    Fixing stupid mistake

--- Metaxas_Pipeline.sh
+++ Metaxas_Pipeline.sh
@@ -133,10 +133,10 @@ do
 
   #cd to idba directory; begin assembly
   cd $BASE_DIR/Metaxas_Output/$filename/idba
-	#Reduce number of iterations for debugging purposes only; REVERT ONCE DONE 
+	#Reduce number of iterations for debugging purposes only; REVERT ONCE DONE
   idba_ud --mink 40 --maxk 60 --step 20 --min_contig 500 -l $filename_wpath --num_threads $THREADS
 	mv out/contig.fa .
-	mv contig.fa out/$filename-contigs.fa
+	mv contig.fa $filename-contigs.fa
 
 	#Fix fasta names for later use with Anvio
 	anvi-script-reformat-fasta $filename-contigs.fa -o $filename-contigs-fixed.fa
@@ -146,10 +146,6 @@ do
 	mv $filename-contigs-fixed.fa $filename-contigs.fa
 
 	cp $filename-contigs.fa ..
-
-
-
-
 
 	#Move to bowtie directory
   cp ../$filename-contigs.fa $BASE_DIR/Metaxas_Output/$filename/bowtie
